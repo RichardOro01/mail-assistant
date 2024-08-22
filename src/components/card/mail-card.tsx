0bf6dc5e7801f24c9a4a2b451f0279ada3c4d36d
isabella.gl <isabella.gl@avangenio.com>
2024-08-22 13:04:52 -0400

    fix: fetch all emails from a conversation

--- src/components/card/mail-card.tsx
+++ src/components/card/mail-card.tsx
@@ -3,6 +3,8 @@ import { IConversation } from "@/types/email";
 import { format } from "date-fns";
 import { Check, Clock, Ellipsis, Trash } from "lucide-react";
 import { useParams } from "next/navigation";
+import DOMPurify from "dompurify";
+import { Separator } from "../ui/separator";
 
 type MailCardProps = {
   conversation: IConversation | undefined;
@@ -23,19 +25,29 @@ const MailCard: React.FC<MailCardProps> = ({ conversation }) => {
           <Trash color="gray" size={18} />
         </div>
       </div>
-      <div className="flex flex-col justify-between h-1/2 w-full shadow-lg border-l-4 border-slate-400 rounded-lg lg:min-w-[400px]">
+      <div className="flex flex-col justify-between shadow-lg border-l-4 border-slate-400 rounded-lg lg:min-w-[400px]">
         <div className="flex flex-col m-5 gap-5">
-          <div className="flex justify-between">
-            <h2 className="font-semibold text-lg">
-              {conversation?.emails[0].from.name}
-            </h2>
-            <p className="opacity-50 text-lg">
-              {conversation?.emails[0].date
-                ? format(conversation.emails[0].date, "hh:mm a")
-                : ""}
-            </p>
-          </div>
-          <div className="">{conversation?.emails[0].text}</div>
+          {conversation?.emails.map((email, index) => (
+            <div key={email.id}>
+              <div className="flex justify-between">
+                <h2 className="font-semibold text-lg">{email.from.name}</h2>
+                <p className="opacity-50 text-lg">
+                  {email.date ? format(email.date, "hh:mm a") : ""}
+                </p>
+              </div>
+              <div
+                dangerouslySetInnerHTML={{
+                  __html: `${DOMPurify.sanitize(
+                    conversation ? email.html : ""
+                  )}`,
+                }}
+                className="mt-2 text-sm"
+              ></div>
+              {index !== conversation?.emails.length - 1 && (
+                <Separator className="mt-6" />
+              )}
+            </div>
+          ))}
         </div>
         <div className="ml-5">
           <button>
