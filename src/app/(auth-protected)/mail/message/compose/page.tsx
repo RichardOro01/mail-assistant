4d4dafde08868f52dc551d6012ecb8583f958deb
Richard Oropesa <richardoro2001@gmail.com>
2024-12-20 12:09:16 -0500

    feat: page titles

--- src/app/(auth-protected)/mail/message/compose/page.tsx
+++ src/app/(auth-protected)/mail/message/compose/page.tsx
@@ -1,6 +1,10 @@
 import { debugRendering } from '@/lib/debug';
 import MailMessageComposeView from '@/sections/mail/message/compose/mail-message-compose-view';
 
+export const metadata = {
+  title: 'Mail Assistant - Compose'
+};
+
 const MailComposePage = () => {
   debugRendering('MailComposePage');
   return <MailMessageComposeView />;
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,6 +1,11 @@
 import { debugRendering } from '@/lib/debug';
 import LoginView from '@/sections/login/login-view';
+import { Metadata } from 'next';
 import React from 'react';
+
+export const metadata: Metadata = {
+  title: 'Mail Assistant - Login'
+};
 
 const LoginPage = async () => {
   debugRendering('LoginPage');
