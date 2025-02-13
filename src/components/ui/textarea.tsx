2596b29001fdff33abba256847b4975bc5395f9c
Richard Oropesa <richardoro2001@gmail.com>
2025-02-13 13:46:35 -0500

    fix: min text area height to 156 px

--- src/components/ui/textarea.tsx
+++ src/components/ui/textarea.tsx
@@ -32,7 +32,7 @@ const Textarea = React.forwardRef<HTMLTextAreaElement, TextareaProps>(
       <div className='w-full flex flex-col gap-1'>
         <textarea
           className={cn(
-            'flex min-h-[80px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
+            'flex min-h-[156px] w-full rounded-md border border-input bg-background px-3 py-2 text-sm ring-offset-background focus-visible:ring-0 focus-visible:ring-offset-0 placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50',
             className
           )}
           ref={textAreaRef}
